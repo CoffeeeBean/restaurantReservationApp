4a912979a5d3e4e4c4f0d9585bb9320227ed9d22
Lucia Pironi <lucia.pironi@guccidigital.com>
2023-12-22 13:00:15 +0100

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontSize: { 
+      "2xsm": "10px",
+      xsm: "12px",
+      sm: "14px",
+      reg: "16px",
+      log: "18px",
+      "2xl": "20px",
+      "3xl": "22px",
+      "4xl": "24px",
+      "5xl": "26px",
+      "6xl": "28px",
+      "7xl": "30px",
+    },
+  },
+  plugins: [],
+}
